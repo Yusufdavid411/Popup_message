c08668107b3bdf90f9b56c397bfe62da57e5fb7d
yusuf david <yusufdavid@gmail.com>
2023-07-03 01:31:55 +0100

    popup_message

---
+++ script.js
@@ -0,0 +1,25 @@
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
