b6eb7a929cbfac3214e7795d2ff8c42228aba54b
yusuf david <yusufdavid@gmail.com>
2023-07-03 11:46:18 +0100

    popup

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 const popup = document.getElementById("popup");
-// const emails = document.getElementById("email");
-// const para = document.querySelector("p");
-
-
-// const x = "yudufdavid@gmail.com";
-
-// if (x.include("@")){
-//     para.innertext = "ok";
-// }
-
-
-
-
 
 function openPopup(){
     popup.classList.add("open-popup");
