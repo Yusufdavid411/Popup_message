dc34a7f969060108a9930e888c46e0ac2b3f997f
yusuf david <yusufdavid@gmail.com>
2023-07-03 04:34:38 +0100

    popup_message

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let popup = document.getElementById("popup");
+const popup = document.getElementById("popup");
+// const emails = document.getElementById("email");
+// const para = document.querySelector("p");
+
+
+// const x = "yudufdavid@gmail.com";
+
+// if (x.include("@")){
+//     para.innertext = "ok";
+// }
+
+
+
+
 
 function openPopup(){
     popup.classList.add("open-popup");
